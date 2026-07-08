6f5a68bc51d352deac0a392220fb442e5e5b077d
Fatime <fabdulezimova2009@gmail.com>
2026-07-08 13:48:15 +0400

    Add admin authentication

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const path = require("path");
+
+const adminRoutes = require("./routes/admin.routes");
 
 const app = express();
 
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Static uploads
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+
+// Routes
 app.get("/", (req, res) => {
-    res.send("VibeBook Backend Running");
+  res.send("VibeBook Backend Running");
 });
+
+app.use("/api/admin", adminRoutes);
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 
 const app = require("./app");
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
